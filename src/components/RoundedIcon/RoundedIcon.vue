62071f7b5594adb663689cc51dc8f90d28783902
Arm <69001162+arskiir@users.noreply.github.com>
2021-09-19 00:17:50 +0700

    ✨ <RoundedIcon /> component

---
+++ src/components/RoundedIcon/RoundedIcon.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const props = defineProps<{
+  imgSrc: string
+  imgAlt: string
+}>()
+
+defineEmits<{
+  (e: 'click'): void
+}>()
+</script>
+
+<template>
+  <div class="rounded-10px w-8 h-8 bg-light-800 object-center overflow-hidden cursor-pointer" @click="$emit('click')">
+    <img :src="props.imgSrc" :alt="props.imgAlt" />
+  </div>
+</template>
+
+<style>
+</style>
