74bfb91a65120955bdab7de9691f070daf9c0039
Arm <69001162+arskiir@users.noreply.github.com>
2021-09-19 01:28:41 +0700

    ♻️ refactor result page, splitting codes

---
+++ src/components/pages/result/ResultButtonGroup.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const replay = () => {
+
+}
+const knowMore = () => {
+
+}
+</script>
+
+<template>
+  <div class="flex justify-evenly">
+    <Button class="w-30" @click="replay">
+      <span class="text-sm">เล่นอีกรอบ</span>
+    </Button>
+    <Button class="w-30" @click="knowMore">
+      <span class="text-sm">รู้จักเขาให้มากขึ้น</span>
+    </Button>
+  </div>
+</template>
+
+<style>
+</style>
---
+++ src/components/pages/result/ResultIconGroup.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+
+const icon1OnClick = () => {
+
+}
+const icon2OnClick = () => {
+
+}
+const icon3OnClick = () => {
+
+}
+
+const shareIcon1Src = 'https://pbs.twimg.com/profile_images/1180519637328502784/EbolKHV8_400x400.png'
+const shareIcon2Src = 'https://img.wongnai.com/p/256x256/2015/06/11/5489a807f7e44d7992c4641601402882.jpg'
+const shareIcon3Src = 'https://pbs.twimg.com/profile_images/1350525503740862465/04a5AOWU.jpg'
+</script>
+
+<template>
+  <div class="space-x-4 flex items-center">
+    <span class="text-lg">แชร์:</span>
+    <RoundedIcon :img-src="shareIcon1Src" :img-alt="'cat 1'" @click="icon1OnClick" />
+    <RoundedIcon :img-src="shareIcon2Src" :img-alt="'cat 2'" @click="icon2OnClick" />
+    <RoundedIcon :img-src="shareIcon3Src" :img-alt="'cat 3'" @click="icon3OnClick" />
+  </div>
+</template>
+
+<style>
+
+</style>
