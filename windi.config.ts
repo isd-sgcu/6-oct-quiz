d791d0b24ffa1ca38f910c1cd496539d9c2b8b71
Arm <69001162+arskiir@users.noreply.github.com>
2021-09-18 19:26:59 +0700

    💄 config for colors

--- windi.config.ts
+++ windi.config.ts
@@ -6,7 +6,48 @@ export default defineConfig({
   attributify: true,
 
   plugins: [],
+
   theme: {
-    extend: {},
+    colors: {
+      black: {
+        DEFAULT: '#020202',
+      },
+      cream: {
+        DEFAULT: '#E2D195',
+      },
+      red: {
+        DEFAULT: '#EC371F',
+        750: '#8C1B0C',
+      },
+      yellow: {
+        DEFAULT: '#FFAE11',
+      },
+      purple: {
+        DEFAULT: '#9F79B7',
+      },
+      cre: {
+        DEFAULT: '#F8E39B',
+      },
+      white: {
+        DEFAULT: '#FCFBF5',
+      },
+    },
+    backgroundColor: {
+      grad: {
+        // e.g. background-grad-1
+        1: 'linear-gradient(180deg, #FFA751 0%, #FFE259 34.38%, #FFFFFF 100%)',
+        2: 'linear-gradient(180deg, #FFA043 0%, #FFD159 34.38%, #FFFFFF 100%)',
+        3: 'linear-gradient(180deg, #FF9243 0%, #FFBD59 34.38%, #FFFFFF 100%)',
+        4: 'linear-gradient(180deg, #FE8228 0%, #FFBD59 52.4%, #FFFFFF 100%)',
+        5: 'linear-gradient(180deg, #FE7528 0%, #FFB359 65.42%, #FFE872 100%)',
+        6: 'linear-gradient(180deg, #FE5B28 10.73%, #FFB359 79.48%, #FFE872 100%)',
+        7: 'linear-gradient(180deg, #C95632 10.73%, #FFBD59 79.48%, #FFE872 100%)',
+        8: 'linear-gradient(180deg, #844B2C 10.73%, #FFBD59 79.48%, #FFE872 100%)',
+        9: 'linear-gradient(180deg, #524E3A 10.73%, #E1D078 100%)',
+        10: 'linear-gradient(180deg, #2E413D 0%, #CBCAA5 100%)',
+      },
+    },
+    extend: {
+    },
   },
 })
