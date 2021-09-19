fde4513b55583c097f2039f806060264677211a2
miello <partearth1212@gmail.com>
2021-09-19 14:42:18 +0700

    🚧 Implement draft for Navbar

--- src/components/Nav/Nav.vue
+++ src/components/Nav/Nav.vue
@@ -2,5 +2,20 @@
 </script>
 
 <template>
-  <nav class="😎"></nav>
+  <div class="flex items-center justify-between px-5 pt-3 pb-3">
+    <div>
+      <img src="https://via.placeholder.com/150" alt="logo" class="w-[150px] h-[50px]">
+    </div>
+    <div class="flex">
+      <p class="mx-3">
+        Hello World 1
+      </p>
+      <p class="mx-3">
+        Hello World 2
+      </p>
+      <p class="mx-3">
+        Hello World 3
+      </p>
+    </div>
+  </div>
 </template>
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="min-h-screen">
     <Nav />
     <main class="bg-white min-h-screen">
       <router-view />
