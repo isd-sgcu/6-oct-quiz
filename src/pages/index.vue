a5ecfb0322b3212bf8ec3b47e1b46f40331b1679
miello <partearth1212@gmail.com>
2021-09-20 00:23:27 +0700

    💄 Add background color for index page and title

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,32 +1,66 @@
 <template>
-  <div>
-    <HeaderText>
-      การเดินทางขากลับคงจะเหงา
-    </HeaderText>
-    <ParagraphText>
-      การเดินทางขากลับคงจะเหงาdfafsdfsd
-    </ParagraphText>
-    dsda
-  </div>
-  <div class="absolute bottom-0 w-full overflow-hidden">
-    <div class="white-fill" />
+  <div class="flex justify-center">
+    <div class="items-center z-10 align-middle m-20 text-center">
+      <p class="topic">
+        คุณคือใครในเหตุการณ์
+      </p>
+      <div class="red-circle">
+        <span class="p-5">6 ตุลา</span>
+      </div>
+    </div>
+    <div class="absolute bottom-0 w-full overflow-hidden">
+      <div class="white-fill" />
+    </div>
   </div>
 </template>
 
 <style scoped>
-@keyframes move {
+/* @-webkit-keyframes displace {
   from {
-    transform: translateX(0%);
+    background-position: 0 center;
   }
   to {
-    transform: translateX(100%);
+    background-position: 100vw center;
   }
 }
+@keyframes displace {
+  from {
+    background-position: 0 center;
+  }
+  to {
+    background-position: 100vw center;
+  }
+} */
 .white-fill {
-  /* See this for more info https://codepen.io/sosuke/pen/Pjoqqp */
   @apply h-[300px] w-full;
   filter: brightness(0) invert(1);
-  background-image: url('../assets/city.svg');
-  animation: move 5s infinite linear;
+  background: url('../assets/city.svg') repeat-x left center;
+  /* animation: displace 2s linear infinite;
+  -webkit-animation: displace 2s linear infinite; */
 }
+
+.topic {
+  font-size: 3.875rem;
+  display: inline-block;
+  margin-top: auto;
+}
+
+.red-circle {
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1;
+  color: white;
+  font-size: 3.875rem;
+  border-radius: 50%;
+  background-color: red;
+}
+
+.red-circle::before {
+  content:'';
+  float: left;
+  width: auto;
+  padding-bottom: 100%;
+}
+
 </style>
