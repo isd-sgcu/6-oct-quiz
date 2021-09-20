5ba97f9b913d53c3944a6eb9dccfc8135d4e5093
miello <partearth1212@gmail.com>
2021-09-20 20:52:26 +0700

    💄 Change arrow direction of back button

--- src/components/Nav/Sidebar.vue
+++ src/components/Nav/Sidebar.vue
@@ -11,7 +11,7 @@ defineProps<IProps>()
 <template>
   <div class="fixed top-0 left-0 bottom-0 right-0 lg:(hidden) bg-white z-20 transition-all" :class="isOpen ? 'open': 'close'">
     <div class="relative p-10 h-full">
-      <mdi-keyboard-backspace class="absolute text-size-[2rem] cursor-pointer" @click="handleClose" />
+      <carbon-arrow-right class="absolute text-size-[2rem] cursor-pointer" @click="handleClose" />
       <div class="flex justify-center items-center flex-col h-full justify-evenly">
         <router-link v-for="item in NavPath" :key="item.title" :to="item.link">
           <ParagraphText class="mx-3 text-size-[1.5rem]">
