c4b70fee602fd8924e8900a08c4801d3aeef1085
miello <partearth1212@gmail.com>
2021-09-20 17:53:48 +0700

    📱 Implement sidebar draft and handle responsive

--- src/components/Nav/Sidebar.vue
+++ src/components/Nav/Sidebar.vue
@@ -1,4 +1,19 @@
 <template>
-  <div class="fixed top-0 left-0 bottom-0 right-0 md:(hidden)">
+  <div class="fixed top-0 left-0 bottom-0 right-0 lg:(hidden) bg-white z-20">
+    <div class="relative p-10 h-full">
+      <mdi-keyboard-backspace class="absolute text-size-[2rem] cursor-pointer" />
+      <div class="flex justify-center items-center flex-col h-full justify-evenly">
+        <ParagraphText class="mx-3">
+          Hello World 1
+        </ParagraphText>
+        <ParagraphText class="mx-3">
+          Hello World 2
+        </ParagraphText>
+
+        <ParagraphText class="mx-3">
+          Hello World 3
+        </ParagraphText>
+      </div>
+    </div>
   </div>
 </template>
