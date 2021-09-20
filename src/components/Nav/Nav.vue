e193f06405c85c5356a9998530666b601580a5f3
miello <partearth1212@gmail.com>
2021-09-20 17:53:48 +0700

    📱 Implement sidebar draft and handle responsive

--- src/components/Nav/Nav.vue
+++ src/components/Nav/Nav.vue
@@ -6,16 +6,21 @@
     <div>
       <img src="https://via.placeholder.com/150" alt="logo" class="w-[150px] h-[50px]">
     </div>
-    <div class="flex">
+    <div class="flex <lg:hidden">
       <p class="mx-3">
         Hello World 1
       </p>
       <p class="mx-3">
         Hello World 2
       </p>
+
       <p class="mx-3">
         Hello World 3
       </p>
     </div>
+    <div class="lg:hidden">
+      <cil-hamburger-menu class="text-size-[2rem] cursor-pointer" />
+    </div>
+    <Sidebar />
   </div>
 </template>
