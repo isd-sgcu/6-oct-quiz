d6b8ae1fb6573fa2615b79a5c2d28b4ea4ca8ce8
Arm <69001162+arskiir@users.noreply.github.com>
2021-09-22 00:52:43 +0700

    🍻 yolo

--- src/components/ResultPicture/ResultPicture.vue
+++ src/components/ResultPicture/ResultPicture.vue
@@ -4,7 +4,7 @@
 //   imgAlt: string
 // }>()
 
-const dumbImg = 'https://images.hindustantimes.com/rf/image_size_630x354/HT/p2/2019/08/08/Pictures/_6bda0940-b9ad-11e9-98cb-e738ad509720.jpg'
+const dumbImg = 'https://c.tenor.com/mljLgH4Csu0AAAAd/cat-cat-vibing.gif'
 </script>
 
 <template>
