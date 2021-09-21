ea0c0f2cbed369c5623d59ccc9b843055c808851
miello <partearth1212@gmail.com>
2021-09-21 17:54:12 +0700

    ✨ Add circle background and some dummy text

--- src/pages/intro.vue
+++ src/pages/intro.vue
@@ -1,37 +1,55 @@
 <template>
-  <div>
-    <div class="flex justify-start pb-20">
-      <div class="max-w-[500px]">
-        <HeaderText class="mb-5 text-size-[3rem] <md:text-size-[2rem]">
-          คำอธิบาย 1
-        </HeaderText>
-        <ParagraphText class="text-size-[1.5rem] <md:text-size-[1rem]">
-          เรซิน บาบูนแหววเอ๋อ ไพลินตี๋อุปนายิกาซีเรียส ซีเนียร์ธรรมาสงบสุข ซูมถ่ายทำเอ็กซ์เพรสโรลออน เบอร์เกอร์แจม เย้วเช็งเม้ง ซินโดรมบลูเบอร์รี่แกสโซฮอล์ ดีพาร์ตเมนต์แฟรนไชส์ม้าหินอ่อนลิมิต นินจาช็อปเปอร์เฟอร์รี่ เรซิน บาบูนแหววเอ๋อ ไพลินตี๋อุปนายิกาซีเรียส ซีเนียร์ธรรมาสงบสุข ซูมถ่ายทำเอ็กซ์เพรสโรลออน เบอร์เกอร์แจม
-        </ParagraphText>
+  <div class="flex justify-center pt-20">
+    <div class="z-10 flex flex-col w-full self-center">
+      <div v-for="(item, key) in dummyData" :key="key" class="flex justify-start pb-20 relative overflow-hidden px-20 <md:px-10" :class="!(key & 1) ? 'justify-start': 'justify-end'">
+        <div class="max-w-[1000px] w-full items-end">
+          <div class="max-w-[500px]">
+            <HeaderText class="mb-5 text-size-[3rem] <md:text-size-[2rem]">
+              {{ item.title }}
+            </HeaderText>
+            <ParagraphText class="leading-10 text-size-[1.5rem] <md:(text-size-[1rem] leading-5)">
+              {{ item.details }}
+            </ParagraphText>
+          </div>
+        </div>
+        <div class="absolute w-[400px] h-[400px] overflow-hidden color-circle <md:(w-[200px] h-[200px]) rounded-full" :class="!(key & 1) ? 'left-[-200px] <md:left-[-100px]': 'right-[-200px] <md:right-[-100px]'" style="z-index: -1;" :style="{ backgroundColor: item.bgColor}">
+        </div>
       </div>
-    </div>
-    <div class="flex justify-end pb-20">
-      <div class="max-w-[500px]">
-        <HeaderText class="mb-5 text-size-[3rem] <md:text-size-[2rem]">
-          คำอธิบาย 2
-        </HeaderText>
-        <ParagraphText class="text-size-[1.5rem] <md:text-size-[1rem]">
-          เรซิน บาบูนแหววเอ๋อ ไพลินตี๋อุปนายิกาซีเรียส ซีเนียร์ธรรมาสงบสุข ซูมถ่ายทำเอ็กซ์เพรสโรลออน เบอร์เกอร์แจม เย้วเช็งเม้ง ซินโดรมบลูเบอร์รี่แกสโซฮอล์ ดีพาร์ตเมนต์แฟรนไชส์ม้าหินอ่อนลิมิต นินจาช็อปเปอร์เฟอร์รี่ เรซิน บาบูนแหววเอ๋อ ไพลินตี๋อุปนายิกาซีเรียส ซีเนียร์ธรรมาสงบสุข ซูมถ่ายทำเอ็กซ์เพรสโรลออน เบอร์เกอร์แจม
+      <Button class="w-full max-w-[200px] self-center bg-[#FCFBF5] hover:bg-cream transition-colors" @click="$router.push('/story')">
+        <ParagraphText>
+          เริ่มการทดสอบ
         </ParagraphText>
-      </div>
-    </div>
-    <div class="flex justify-start pb-20">
-      <div class="max-w-[500px]">
-        <HeaderText class="mb-5 text-size-[3rem] <md:text-size-[2rem]">
-          คำอธิบาย 3
-        </HeaderText>
-        <ParagraphText class="text-size-[1.5rem] <md:text-size-[1rem]">
-          เรซิน บาบูนแหววเอ๋อ ไพลินตี๋อุปนายิกาซีเรียส ซีเนียร์ธรรมาสงบสุข ซูมถ่ายทำเอ็กซ์เพรสโรลออน เบอร์เกอร์แจม เย้วเช็งเม้ง ซินโดรมบลูเบอร์รี่แกสโซฮอล์ ดีพาร์ตเมนต์แฟรนไชส์ม้าหินอ่อนลิมิต นินจาช็อปเปอร์เฟอร์รี่ เรซิน บาบูนแหววเอ๋อ ไพลินตี๋อุปนายิกาซีเรียส ซีเนียร์ธรรมาสงบสุข ซูมถ่ายทำเอ็กซ์เพรสโรลออน เบอร์เกอร์แจม
-        </ParagraphText>
-      </div>
+      </Button>
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+const dummyData = [
+  {
+    title: 'คำอธิบาย 1',
+    details: 'เรซิน บาบูนแหววเอ๋อ ไพลินตี๋อุปนายิกาซีเรียส ซีเนียร์ธรรมาสงบสุข ซูมถ่ายทำเอ็กซ์เพรสโรลออน เบอร์เกอร์แจม เย้วเช็งเม้ง ซินโดรมบลูเบอร์รี่แกสโซฮอล์ ดีพาร์ตเมนต์แฟรนไชส์ม้าหินอ่อนลิมิต นินจาช็อปเปอร์เฟอร์รี่ เรซิน บาบูนแหววเอ๋อ ไพลินตี๋อุปนายิกาซีเรียส ซีเนียร์ธรรมาสงบสุข ซูมถ่ายทำเอ็กซ์เพรสโรลออน เบอร์เกอร์แจม',
+    bgColor: '#FFAE11',
+  },
+  {
+    title: 'คำอธิบาย 2',
+    details: 'เรซิน บาบูนแหววเอ๋อ ไพลินตี๋อุปนายิกาซีเรียส ซีเนียร์ธรรมาสงบสุข ซูมถ่ายทำเอ็กซ์เพรสโรลออน เบอร์เกอร์แจม เย้วเช็งเม้ง ซินโดรมบลูเบอร์รี่แกสโซฮอล์ ดีพาร์ตเมนต์แฟรนไชส์ม้าหินอ่อนลิมิต นินจาช็อปเปอร์เฟอร์รี่ เรซิน บาบูนแหววเอ๋อ ไพลินตี๋อุปนายิกาซีเรียส ซีเนียร์ธรรมาสงบสุข ซูมถ่ายทำเอ็กซ์เพรสโรลออน เบอร์เกอร์แจม',
+    bgColor: '#EC371F',
+  },
+  {
+    title: 'คำอธิบาย 3',
+    details: 'เรซิน บาบูนแหววเอ๋อ ไพลินตี๋อุปนายิกาซีเรียส ซีเนียร์ธรรมาสงบสุข ซูมถ่ายทำเอ็กซ์เพรสโรลออน เบอร์เกอร์แจม เย้วเช็งเม้ง ซินโดรมบลูเบอร์รี่แกสโซฮอล์ ดีพาร์ตเมนต์แฟรนไชส์ม้าหินอ่อนลิมิต นินจาช็อปเปอร์เฟอร์รี่ เรซิน บาบูนแหววเอ๋อ ไพลินตี๋อุปนายิกาซีเรียส ซีเนียร์ธรรมาสงบสุข ซูมถ่ายทำเอ็กซ์เพรสโรลออน เบอร์เกอร์แจม',
+    bgColor: '#9F79B7',
+  },
+]
+</script>
+
+<style scoped>
+.color-circle {
+  top: 50%;
+  transform: translateY(-50%);
+}
+</style>
 
 <route lang="yaml">
 meta:
