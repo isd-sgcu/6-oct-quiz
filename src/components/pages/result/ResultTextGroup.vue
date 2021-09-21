92c6b5a95a8cb68c86903f7de9b278c445f40064
Arm <69001162+arskiir@users.noreply.github.com>
2021-09-21 14:30:04 +0700

    💄 for md: 768 px

--- src/components/pages/result/ResultTextGroup.vue
+++ src/components/pages/result/ResultTextGroup.vue
@@ -7,10 +7,10 @@ const props = defineProps<{
 
 <template>
   <div>
-    <HeaderText class="text-2rem whitespace-pre">
+    <HeaderText class="whitespace-pre text-2rem sm:(text-2.3rem)">
       <h1>{{ props.resultFullName }}</h1>
     </HeaderText>
-    <ParagraphText class="mt-3">
+    <ParagraphText class="mt-3 sm:(text-1.1rem leading-7)">
       <p>{{ props.resultDetail }}</p>
     </ParagraphText>
   </div>
