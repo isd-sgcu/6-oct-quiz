77b37facda61e87f752b848115c20f7900839c67
Sahatsarin Pawanna <sahatsarin07@gmail.com>
2021-09-30 01:10:08 +0700

    🎨 Change bg and font weight

--- src/components/Typography/QuestionText.vue
+++ src/components/Typography/QuestionText.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="font-Trirong text-[24px] leading-10 md:text-[36px]">
+  <div class="font-Trirong font-light text-[24px] leading-10 md:text-[36px]">
     <slot />
   </div>
 </template>
--- src/layouts/quiz.vue
+++ src/layouts/quiz.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="bg-cream min-h-screen w-full">
+  <main class="bg-grad-1 min-h-screen w-full">
     <router-view />
   </main>
 </template>
