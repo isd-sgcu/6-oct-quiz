06ccf0aa7799c348878068cbe4e32b8b06a3c5ec
Sahatsarin Pawanna <sahatsarin07@gmail.com>
2021-09-30 15:49:26 +0700

    Create enum for choices of the quizes

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,9 @@
 import { ViteSSGContext } from 'vite-ssg'
 
 export type UserModule = (ctx: ViteSSGContext) => void
+
+export enum QuestionChoice {
+  TotallyYes = 2,
+  NotKnow = 1,
+  Never = 0,
+}
