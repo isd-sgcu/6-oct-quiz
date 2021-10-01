64c95b2a957753a28462375b3f29938c0034a290
miello <partearth1212@gmail.com>
2021-10-01 18:14:35 +0700

    💄 Fix wrong circle height

--- src/pages/intro.vue
+++ src/pages/intro.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex justify-center pt-50 <xl:pt-35 pb-20 overflow-hidden">
+  <div class="flex justify-center pt-50 <xl:pt-40 pb-20 overflow-hidden">
     <div class="z-10 flex flex-col w-full self-center ">
       <div v-for="(item, key) in dummyData" :key="key" class="flex justify-start pb-15 pt-3 relative px-20 <md:px-10" :class="!(key & 1) ? 'justify-start': 'justify-end'">
         <div class="max-w-[1000px] overflow-hidden" :class="(key & 1) && 'flex flex-col items-end text-right'">
@@ -10,7 +10,7 @@
             {{ item.details }}
           </ParagraphText>
         </div>
-        <div class="transition-all color-circle absolute w-[400px] h-[400px] <xl:(w-[350px] h-[300px]) <md:(w-[250px] h-[250px]) rounded-full" :class="!(key & 1) ? 'left-[-200px] <xl:(left-[-175px]) <md:(left-[-125px])': 'right-[-200px] <xl:(right-[-175px]) <md:(right-[-125px])'" style="z-index: -1;" :style="{ backgroundColor: item.bgColor}">
+        <div class="transition-all color-circle absolute w-[400px] h-[400px] <xl:(w-[350px] h-[350px]) <md:(w-[250px] h-[250px]) rounded-full" :class="!(key & 1) ? 'left-[-200px] <xl:(left-[-175px]) <md:(left-[-125px])': 'right-[-200px] <xl:(right-[-175px]) <md:(right-[-125px])'" style="z-index: -1;" :style="{ backgroundColor: item.bgColor}">
         </div>
       </div>
       <Button class="w-full max-w-[300px] max-h-max py-4 mt-15 self-center bg-[#FCFBF5] hover:bg-cream transition-colors" @click="$router.push('/story')">
