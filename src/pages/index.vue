d8145afb21b287d649a5db29dbc02a50b756011a
miello <partearth1212@gmail.com>
2021-10-01 18:19:12 +0700

    🔥 Remove remaining logic

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,13 +1,3 @@
-<script setup lang="ts">
-import { useRouter } from 'vue-router'
-
-const router = useRouter()
-
-const handleSubmit = () => {
-  router.push('/intro')
-}
-</script>
-
 <template>
   <div class="flex justify-center items-center h-full flex-col">
     <div class="z-10 m-10 flex items-center flex-wrap">
@@ -21,7 +11,7 @@ const handleSubmit = () => {
       </div>
     </div>
     <div class="z-10 flex flex-col items-center w-full px-10">
-      <Button class="w-full max-w-[400px] p-6 !py-4 mt-5 bg-white hover:bg-cream transition-colors" @click="handleSubmit">
+      <Button class="w-full max-w-[400px] p-6 !py-4 mt-5 bg-white hover:bg-cream transition-colors" @click="$router.push('/intro')">
         <ParagraphText class="text-size-[1.5rem]">
           เริ่มการทดสอบ
         </ParagraphText>
