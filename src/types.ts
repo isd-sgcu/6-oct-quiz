042b30f5b339e2a49f91f630f87a86a28a6813cd
Arm <69001162+arskiir@users.noreply.github.com>
2021-10-02 23:08:18 +0700

    🚧 added real characters' info

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,11 @@
 import { ViteSSGContext } from 'vite-ssg'
 
 export type UserModule = (ctx: ViteSSGContext) => void
+
+export type CharacterKeyOption = 'ss' | 'al' | 'sb' | 'wa' | 'wd' | 'pw' | 'kl' | 'bp' | 'empty'
+
+export type Character = {
+  fullName: string
+  detail: string
+  url: string
+}
