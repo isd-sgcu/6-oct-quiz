f4ecd9ec68a4bee7fe8facfd8044903ecdfed54c
Sahatsarin Pawanna <sahatsarin07@gmail.com>
2021-10-03 02:38:17 +0700

    🎨 Implement changing background color throughout a quiz

--- src/layouts/quiz.vue
+++ src/layouts/quiz.vue
@@ -1,5 +1,16 @@
 <template>
-  <main class="bg-grad-1 min-h-screen w-full">
+  <main
+    class="sky min-h-screen w-full transition ease-in-out duration-500"
+    :class="'bg-grad-' + qNumber"
+  >
     <router-view />
   </main>
 </template>
+
+<script setup lang="ts">
+import { useGameStore } from '~/stores/game'
+
+const gameStore = useGameStore()
+
+const qNumber = computed(() => gameStore.currentIndex + 1)
+</script>
