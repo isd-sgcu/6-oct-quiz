986bb3a919137fd8e947331732beb02ac31db7f3
miello <partearth1212@gmail.com>
2021-10-02 17:07:57 +0700

    🐛 Fix build failed

--- src/pages/intro.vue
+++ src/pages/intro.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex justify-center pt-20 pb-20 overflow-hidden relative">
     <div class="z-10 flex flex-col w-full self-center <md:px-5">
-      <div v-for="(item, key) in dummyData" :id="`el-${key}`" :key="key" class="flex justify-start pb-15 pt-3 relative px-20 <md:px-0 h-screen items-center transition-all duration-500" :class="`${!(key & 1) ? 'justify-start': 'justify-end'} ${(key === presentNum) ? 'opacity-100': 'opacity-0'}`">
+      <div v-for="(item, key) in info" :id="`el-${key}`" :key="key" class="flex justify-start pb-15 pt-3 relative px-20 <md:px-0 h-screen items-center transition-all duration-1000" :class="`${!(key & 1) ? 'justify-start': 'justify-end'} ${(key === presentNum) ? 'opacity-100': 'opacity-0'}`">
         <div class="max-w-[1000px] overflow-hidden flex flex-col" :class="(key & 1) && 'flex flex-col items-end text-right'">
           <HeaderText class="mb-5 text-size-[3.5rem] leading-20 <xl:(text-size-[3rem] leading-16) <md:(text-size-[2.5rem] leading-12) transition-all">
             {{ item.title }}
@@ -13,21 +13,18 @@
         <div class="transition-all absolute w-[800px] h-[800px] <xl:(w-[750px] h-[750px]) <md:(w-[620px] h-[620px]) rounded-full" :class="!(key & 1) ? 'left-[-400px] <xl:(left-[-375px]) <md:(left-[-310px])': 'right-[-400px] <xl:(right-[-375px]) <md:(right-[-310px])'" style="z-index: -1;" :style="{ backgroundColor: item.bgColor}">
         </div>
       </div>
-      <Button :id="`el-${dummyData.length}`" class="w-full max-w-[300px] max-h-max py-4 self-center bg-[#FCFBF5] hover:bg-cream transition-colors mt-5 " :class="`${(presentNum === dummyData.length - 1) ? 'opacity-100': 'opacity-0'}`" @click="$router.push('/story')">
+      <Button :id="`el-${info.length}`" class="w-full max-w-[300px] max-h-max py-4 self-center bg-[#FCFBF5] hover:bg-cream transition-colors mt-5 " :class="`${(presentNum === info.length - 1) ? 'opacity-100': 'opacity-0'}`" @click="$router.push('/story')">
         <ParagraphText class="text-size-[1.75rem]">
           เริ่มการทดสอบ
         </ParagraphText>
       </Button>
     </div>
-    <mdi-arrow-down-circle-outline class="fixed bottom-10 text-size-[3rem] cursor-pointer z-30 transition-opacity duration-500" :class="isHide ? 'opacity-0': 'opacity-100'" @click="handleScroll" />
+    <mdi-arrow-down-circle-outline class="fixed bottom-10 text-size-[3rem] cursor-pointer transition-opacity duration-500" :class="isHide ? 'opacity-0': 'opacity-100 z-30'" @click="handleScroll" />
   </div>
 </template>
 
 <script setup lang="ts">
-import { elementScrollIntoViewPolyfill, elementScrollIntoView } from 'seamless-scroll-polyfill'
-
-elementScrollIntoViewPolyfill()
-const dummyData = [
+const info = [
   {
     title: 'ในเดือนตุลาฯ หนึ่งของปี ๒๕๑๙',
     details: ' มีเพื่อนเราหลายคนผ่านเหตุการณ์สําคัญหนึ่งของชีวิตมา',
@@ -35,7 +32,7 @@ const dummyData = [
   },
   {
     title: '6 ตุลาฯ',
-    details: ' พรากหลายอย่างจากพวกเขาและเธอไปมากมาย แต่กระนั้นความสูญเสียนี้ก็ให้บทเรียนล้ำค่าและสร้างตัวตนของคนเดือนตุลาฯ มาไม่น้อย เขาและเธอเหล่านี้เลือกเส้นทางประชาธิปไตยอย่างแน่วแน่มาตลอด แต่บางคนก็เปลี่ยนแปลงไป มากบ้างน้อยบ้าง',
+    details: 'พรากหลายอย่างจากพวกเขาและเธอไปมากมาย แต่กระนั้นความสูญเสียนี้ก็ให้บทเรียนล้ำค่าและสร้างตัวตนของคนเดือนตุลาฯ มาไม่น้อย เขาและเธอเหล่านี้เลือกเส้นทางประชาธิปไตยอย่างแน่วแน่มาตลอด แต่บางคนก็เปลี่ยนแปลงไป มากบ้างน้อยบ้าง',
     bgColor: '#EC371F',
   },
   {
@@ -50,7 +47,7 @@ const isHide = ref<boolean>(false)
 let elHeight: number[] = []
 
 onMounted(() => {
-  const elSz = dummyData.length
+  const elSz = info.length
   for (let i = 0; i < elSz; i++) {
     const el = document.getElementById(`el-${i}`)
     if (el)
@@ -81,10 +78,29 @@ onMounted(() => {
   }
 })
 
+function scrollToSmoothly(pos: number, time: number) {
+  const currentPos = window.pageYOffset
+  let start: number | null = null
+  window.requestAnimationFrame(function step(currentTime) {
+    start = !start ? currentTime : start
+    const progress = currentTime - start
+    if (currentPos < pos)
+      window.scrollTo(0, ((pos - currentPos) * progress / time) + currentPos)
+    else
+      window.scrollTo(0, currentPos - ((currentPos - pos) * progress / time))
+
+    if (progress < time)
+      window.requestAnimationFrame(step)
+    else
+      window.scrollTo(0, pos)
+  })
+}
+
 const handleScroll = () => {
   if (isHide.value) return
   const nextIdx = Math.min(elHeight.length, presentNum.value + 1)
   const el = document.getElementById(`el-${nextIdx}`)
-  if (el) elementScrollIntoView(el, { behavior: 'smooth' })
+
+  if (el) scrollToSmoothly(el.offsetTop, 600)
 }
 </script>
