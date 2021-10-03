2d11a42e902a63d5898fcf7cc0a2631f1faf2194
Arm <69001162+arskiir@users.noreply.github.com>
2021-10-03 18:33:57 +0700

    ♻️ refactor setMetadata for easier usage

--- src/pages/share/ss.vue
+++ src/pages/share/ss.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
 import { replay, setMetadata } from '~/utils'
 
-setMetadata(
-  'This is the title',
-  'This is the description. I have things to do but I am lazy so I lay down on my bed with my phone.',
-  'https://i.kym-cdn.com/entries/icons/original/000/034/915/vibingcatttt.gif',
-)
+setMetadata({
+  title: 'คุณคือ สุธรรม แสงประทุม',
+  image: 'https://i.kym-cdn.com/entries/icons/original/000/034/915/vibingcatttt.gif',
+})
 
 replay()
 </script>
