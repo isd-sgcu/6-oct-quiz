a87e9632aa1477de92f569a3bddb0906c42375f6
Arm <69001162+arskiir@users.noreply.github.com>
2021-10-03 15:37:57 +0700

    🐛 This overwrites its child metadata, so get commented

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,12 @@
 // https://github.com/vueuse/head
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
-useHead({
-  title: 'Vitesse',
-  meta: [
-    { name: 'description', content: 'Opinionated Vite Starter Template' },
-  ],
-})
+// useHead({
+//   title: 'Vitesse',
+//   meta: [
+//     { name: 'description', content: 'Opinionated Vite Starter Template' },
+//   ],
+// })
 </script>
 
 <template>
