29b3691346c030aa3f45758c003bc54e96b42a6a
Arm <69001162+arskiir@users.noreply.github.com>
2021-10-03 18:12:03 +0700

    ♻️ refactor setting metadata

--- src/pages/invite/ss.vue
+++ src/pages/invite/ss.vue
@@ -1,34 +1,14 @@
 <script setup lang="ts">
-// import { replay } from '~/utils'
+import { replay, setMetadata } from '~/utils'
 
-const title = 'This is the title'
-const description = 'This is the description. I have things to do but I am lazy so I lay down on my bed with my phone.'
-const url = 'https://this-is-the-website.com'
-const image = 'https://i.kym-cdn.com/entries/icons/original/000/034/915/vibingcatttt.gif'
+setMetadata(
+  'This is the title',
+  'This is the description. I have things to do but I am lazy so I lay down on my bed with my phone.',
+  'https://this-is-the-website.com',
+  'https://i.kym-cdn.com/entries/icons/original/000/034/915/vibingcatttt.gif',
+)
 
-useHead({
-  title,
-  meta: [
-    { name: 'description', content: description },
-
-    { property: 'og:type', content: 'website' },
-    { property: 'og:url', content: url },
-    { property: 'og:title', content: title },
-    { property: 'og:description', content: description },
-    { property: 'og:image', content: image },
-    { property: 'og:locale', content: 'th_TH' },
-    { property: 'og:site_name', content: '6 oct' },
-
-    { property: 'twitter:card', content: 'summary_large_image' },
-    { property: 'twitter:url', content: url },
-    { property: 'twitter:title', content: title },
-    { property: 'twitter:description', content: description },
-    { property: 'twitter:image', content: image },
-
-  ],
-})
-
-// replay()
+replay()
 </script>
 
 <template>
--- src/utils.ts
+++ src/utils.ts
@@ -2,3 +2,31 @@ export const replay = () => {
   const router = useRouter()
   router.replace('/')
 }
+
+export const setMetadata = (
+  title: string,
+  description: string,
+  url: string,
+  image: string,
+  siteName = 'คุณคือใครในเหตุการณ์ 6 ตุลาฯ',
+) => useHead({
+  title,
+  meta: [
+    { name: 'description', content: description },
+
+    { property: 'og:type', content: 'website' },
+    { property: 'og:url', content: url },
+    { property: 'og:title', content: title },
+    { property: 'og:description', content: description },
+    { property: 'og:image', content: image },
+    { property: 'og:locale', content: 'th_TH' },
+    { property: 'og:site_name', content: siteName },
+
+    { property: 'twitter:card', content: 'summary_large_image' },
+    { property: 'twitter:url', content: url },
+    { property: 'twitter:title', content: title },
+    { property: 'twitter:description', content: description },
+    { property: 'twitter:image', content: image },
+
+  ],
+})
