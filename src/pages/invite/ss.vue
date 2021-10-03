78c42a8760d2203839bd15f549dc0183a088fbb2
Arm <69001162+arskiir@users.noreply.github.com>
2021-10-03 15:32:22 +0700

    🚧 first try

--- src/pages/invite/ss.vue
+++ src/pages/invite/ss.vue
@@ -1,22 +1,30 @@
 <script setup lang="ts">
 import { replay } from '~/utils'
 
+const title = 'This is the title'
+const description = 'This is the description. I have things to do but I am lazy so I lay down on my bed with my phone.'
+const url = 'https://this-is-the-website.com'
+const image = 'https://i.kym-cdn.com/entries/icons/original/000/034/915/vibingcatttt.gif'
+
 useHead({
-  title: 'Yo',
+  title,
   meta: [
-    { name: 'description', content: 'This is a description.' },
-    { property: 'og:locale', content: '' },
-    { property: 'og:type', content: '' },
-    { property: 'og:title', content: '' },
-    { property: 'og:description', content: '' },
-    { property: 'og:url', content: '' },
-    { property: 'og:site_name', content: '' },
-    { property: 'og:image', content: '' },
-    { property: 'twitter:card', content: '' },
-    { property: 'twitter:url', content: '' },
-    { property: 'twitter:title', content: '' },
-    { property: 'twitter:description', content: '' },
-    { property: 'twitter:image', content: '' },
+    { name: 'description', content: description },
+
+    { property: 'og:type', content: 'website' },
+    { property: 'og:url', content: url },
+    { property: 'og:title', content: title },
+    { property: 'og:description', content: description },
+    { property: 'og:image', content: image },
+    { property: 'og:locale', content: 'th_TH' },
+    { property: 'og:site_name', content: '6 oct' },
+
+    { property: 'twitter:card', content: 'summary_large_image' },
+    { property: 'twitter:url', content: url },
+    { property: 'twitter:title', content: title },
+    { property: 'twitter:description', content: description },
+    { property: 'twitter:image', content: image },
+
   ],
 })
 
