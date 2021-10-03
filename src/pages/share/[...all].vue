147b0db189309f94409513733e31a793458d29b8
Arm <69001162+arskiir@users.noreply.github.com>
2021-10-03 19:18:15 +0700

    🚧 handle /share/[...all]

---
+++ src/pages/share/[...all].vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { replay, setMetadata } from '~/utils'
+setMetadata({})
+replay()
+</script>
+
+<template>
+  <div></div>
+</template>
+
+<route lang="yaml">
+meta:
+  layout: share
+</route>
