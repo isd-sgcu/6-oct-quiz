eda58043933d613816a92e166bb7157586bf261f
Arm <69001162+arskiir@users.noreply.github.com>
2021-10-03 18:33:57 +0700

    ♻️ refactor setMetadata for easier usage

--- src/utils.ts
+++ src/utils.ts
@@ -3,30 +3,32 @@ export const replay = () => {
   router.replace('/')
 }
 
-export const setMetadata = (
-  title: string,
-  description: string,
+export const setMetadata = ({
+  title = 'คุณคือใครในเหตุการณ์ 6 ตุลาฯ',
+  description = 'เข้าร่วมทำแบบประเมินเพื่อค้นหาว่าคุณคือใครในเหตุการณ์ 6 ตุลาฯ กันเถอะ',
   image = '',
   url = 'https://www.google.com',
   siteName = 'คุณคือใครในเหตุการณ์ 6 ตุลาฯ',
-) => useHead({
-  title,
-  meta: [
-    { name: 'description', content: description },
+}) => {
+  return useHead({
+    title,
+    meta: [
+      { name: 'description', content: description },
 
-    { property: 'og:type', content: 'website' },
-    { property: 'og:url', content: url },
-    { property: 'og:title', content: title },
-    { property: 'og:description', content: description },
-    { property: 'og:image', content: image },
-    { property: 'og:locale', content: 'th_TH' },
-    { property: 'og:site_name', content: siteName },
+      { property: 'og:type', content: 'website' },
+      { property: 'og:url', content: url },
+      { property: 'og:title', content: title },
+      { property: 'og:description', content: description },
+      { property: 'og:image', content: image },
+      { property: 'og:locale', content: 'th_TH' },
+      { property: 'og:site_name', content: siteName },
 
-    { property: 'twitter:card', content: 'summary_large_image' },
-    { property: 'twitter:url', content: url },
-    { property: 'twitter:title', content: title },
-    { property: 'twitter:description', content: description },
-    { property: 'twitter:image', content: image },
+      { property: 'twitter:card', content: 'summary_large_image' },
+      { property: 'twitter:url', content: url },
+      { property: 'twitter:title', content: title },
+      { property: 'twitter:description', content: description },
+      { property: 'twitter:image', content: image },
 
-  ],
-})
+    ],
+  })
+}
