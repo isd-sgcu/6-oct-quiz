163f5437772d3ef495e73beb20d1c6cb82a1bcf5
Ricecamera <sahatsarin07@gmail.com>
2021-10-05 16:41:07 +0700

    Letter loading animation

--- src/pages/pre-result.vue
+++ src/pages/pre-result.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="flex items-center justify-center h-screen w-full flex-col px-8">
-    <QuestionText class="text-white">
-      คุณคือ...
+    <QuestionText class="wave">
+      <span v-for="(letter, index) in text" :style="{'--c':(index + 1) + ''}" >
+        {{ letter }}
+      </span>
     </QuestionText>
   </div>
 </template>
@@ -11,19 +13,46 @@ import { useGameStore } from '~/stores/game'
 
 const game = useGameStore()
 const router = useRouter()
+
+const text = ['คุ', 'ณ', 'คื', 'อ', ' ', '.' , '.', '.']
 try {
   const characterkey = game.determineCharacter()
   game.reset()
 
   setTimeout(() => {
     router.push(`/result/${characterkey}`)
-  }, 1500)
+  }, 3000)
 }
 catch (error) {
   console.log(error.message)
   router.replace('/story')
 }
 </script>
+
+<style>
+.wave{
+  position: relative;
+}
+.wave span{
+  position: relative;
+  display: inline-block;
+  color:#fff;
+  animation:animate 1.5s ease-in-out infinite;
+  animation-delay: calc(.1s * var(--c));
+}
+
+@keyframes animate{
+  0%,100%{
+    transform: translateY(0px);
+  }
+  20%{
+    transform: translateY(-20px);
+  }
+  40%{
+    transform: translateY(0px);
+  }
+}
+</style>
 
 <route lang="yaml">
 meta:
