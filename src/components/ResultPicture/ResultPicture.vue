23d31a5120c987c8ef2c96b90000cdc51a12e30f
Arm <69001162+arskiir@users.noreply.github.com>
2021-10-08 20:04:53 +0700

    💄 resized images on large screens

--- src/components/ResultPicture/ResultPicture.vue
+++ src/components/ResultPicture/ResultPicture.vue
@@ -7,12 +7,8 @@ const props = defineProps<{
 
 <template>
   <div>
-    <div class="relative w-55 h-55 xs:(w-60 h-60) sm:(w-70 h-70) lg:(w-100 h-100) 2xl:(w-120 h-120)">
-      <div
-        class=" rounded-full z-2 overflow-hidden"
-      >
-        <img class="h-full object-center object-cover" :src="props.image" :alt="props.alt" />
-      </div>
+    <div class="relative w-55 h-55 xs:(w-60) sm:(w-70) lg:(w-100) xl:(w-110) 2xl:(w-120)">
+      <img class="w-full" :src="props.image" :alt="props.alt" />
     </div>
   </div>
 </template>
