b8dc08eea991b5a35337e8f7740abaa3228d5c04
miello <partearth1212@gmail.com>
2021-10-09 15:24:59 +0700

    💄 Make new design for desktop size

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -7,23 +7,37 @@ setMetadata({
 </script>
 
 <template>
-  <div class="flex pt-5 justify-center items-center h-full flex-col">
-    <div class="z-10 m-10 flex md:(justify-center) <md:(justify-end) items-center flex-wrap">
-      <HeaderText class="font-Prachachon topic">
-        คุณคือใครในเหตุการณ์เดือน
-      </HeaderText>
-      <div class="red-circle">
-        <HeaderText class="font-Prachachon p-5">
-          ตุลาฯ
-        </HeaderText>
+  <div class="flex pt-5 h-full flex-col">
+    <div class="z-10 m-10 flex justify-center items-center flex-wrap">
+      <div class="flex flex-col justify-center items-center">
+        <div class="w-full">
+          <div class="font-Prachachon topic">
+            คุณคือใคร
+          </div>
+        </div>
+        <div class="relative md:flex">
+          <div class="flex flex-col justify-center items-center flex-wrap">
+            <div class="topic font-Prachachon">
+              ในเหตุการณ์เดือน
+            </div>
+            <Button class="w-full max-w-[300px] md:inline hidden p-6 !py-4 mt-5 bg-white hover:bg-cream transition-colors" @click="$router.push('/intro')">
+              <div class="text-size-[1.5rem] font-bold">
+                Start
+              </div>
+            </Button>
+          </div>
+          <div class="red-circle">
+            <div class="font-Prachachon p-5">
+              ตุลาฯ
+            </div>
+          </div>
+        </div>
+        <Button class="w-full max-w-[200px] md:(hidden) p-6 !py-4 mt-12 bg-white hover:bg-cream transition-colors" @click="$router.push('/intro')">
+          <div class="text-size-[1.5rem] font-bold">
+            Start
+          </div>
+        </Button>
       </div>
-    </div>
-    <div class="z-10 flex flex-col items-center w-full px-10">
-      <Button class="w-full max-w-[400px] p-6 !py-4 mt-5 bg-white hover:bg-cream transition-colors" @click="$router.push('/intro')">
-        <ParagraphText class="text-size-[1.5rem] font-bold">
-          Start
-        </ParagraphText>
-      </Button>
     </div>
   </div>
 </template>
@@ -31,12 +45,12 @@ setMetadata({
 <style scoped>
 
 .topic {
-  @apply z-0 text-size-[3.5rem] <md:text-size-[3rem] text-white mr-3;
+  @apply z-0 text-size-[6rem] <md:text-size-[4rem] text-white mr-3;
   display: inline-block;
 }
 
 .red-circle {
-  @apply text-size-[3.5rem] <md:(text-size-[3rem] -mt-15);
+  @apply text-size-[6rem] <md:(text-size-[4rem]);
   display: inline-flex;
   justify-content: center;
   align-items: center;
