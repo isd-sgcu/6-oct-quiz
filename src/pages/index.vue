b9bed77a5a613944a34ebda8ab207bba1add4ae5
miello <partearth1212@gmail.com>
2021-10-09 12:49:02 +0700

    💄 Adjust element alignment

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -8,15 +8,15 @@ setMetadata({
 
 <template>
   <div class="flex pt-5 justify-center items-center h-full flex-col">
-    <div class="z-10 m-10 flex items-center flex-wrap">
+    <div class="z-10 m-10 flex justify-center items-center flex-wrap">
       <HeaderText class="font-Prachachon topic">
         คุณคือใครในเหตุการณ์เดือน
-        <div class="red-circle">
-          <HeaderText class="font-Prachachon p-5">
-            ตุลาฯ
-          </HeaderText>
-        </div>
       </HeaderText>
+      <div class="red-circle">
+        <HeaderText class="font-Prachachon p-5">
+          ตุลาฯ
+        </HeaderText>
+      </div>
     </div>
     <div class="z-10 flex flex-col items-center w-full px-10">
       <Button class="w-full max-w-[400px] p-6 !py-4 mt-5 bg-white hover:bg-cream transition-colors" @click="$router.push('/intro')">
@@ -31,12 +31,12 @@ setMetadata({
 <style scoped>
 
 .topic {
-  @apply z-0 text-size-[4.8rem] <md:text-size-[3rem] text-white;
+  @apply z-0 text-size-[3.5rem] <md:text-size-[3rem] text-white mr-3;
   display: inline-block;
 }
 
 .red-circle {
-  @apply text-size-[4.8rem] <md:text-size-[3rem] ;
+  @apply text-size-[3.5rem] <md:text-size-[3rem];
   display: inline-flex;
   justify-content: center;
   align-items: center;
