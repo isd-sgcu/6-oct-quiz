893d0bf94864d55a23a4011879d58c6867bff6d2
Arm <69001162+arskiir@users.noreply.github.com>
2021-10-10 00:45:38 +0700

    🥚 hexToRgb array function

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,14 @@
 import characters from './assets/characters'
 import { CharacterKeyOption } from './types'
 
+export const hexToRgb = (hex: string) => {
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
+  if (!result) throw new Error('Invalid hex color')
+  return [
+    parseInt(result[1], 16), parseInt(result[2], 16), parseInt(result[3], 16),
+  ]
+}
+
 export const goToIndex = () => {
   const router = useRouter()
   router.replace('/')
