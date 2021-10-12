332a2fdda347af2f10d38bcd8c245c97a8c59cbf
Arm <69001162+arskiir@users.noreply.github.com>
2021-10-12 12:01:46 +0700

    🚧 logic for showing spinner

--- src/components/ResultPicture/ResultPicture.vue
+++ src/components/ResultPicture/ResultPicture.vue
@@ -3,12 +3,19 @@ const props = defineProps<{
   image: string
   alt: string
 }>()
+
+const isLoaded = ref(false)
+
+const show = () => {
+  isLoaded.value = true
+}
 </script>
 
 <template>
   <div>
     <div class="mx-auto w-[80%] py-10 xs:(w-[70%]) sm:(w-100) lg:(w-90 py-0) xl:(w-110) 2xl:(w-120)">
-      <img class="w-full" :src="props.image" :alt="props.alt" />
+      <div v-show="!isLoaded" class="w-full h-full bg-red">f</div>
+      <img v-show="isLoaded" class="w-full" :src="props.image" :alt="props.alt" @load="show" />
     </div>
   </div>
 </template>
