4f521fbd345fddaf65e8034f795892f0a6382468
Arm <69001162+arskiir@users.noreply.github.com>
2021-10-12 18:00:12 +0700

    ✨ fade in result text group

--- src/components/pages/result/ResultTextGroup.vue
+++ src/components/pages/result/ResultTextGroup.vue
@@ -10,17 +10,23 @@ const props = defineProps<{
 <template>
   <div>
     <div>
-      <h1 class="font-Prachachon whitespace-pre text-7xl leading-13 sm:(text-5rem leading-15) lg:(text-7xl) xl:(text-8xl) 2xl:(text-9xl)">
-        {{ props.resultFullName }}
-      </h1>
-      <ParagraphText class="kjfhsdkfhksjdfhkjshwuef">
-        <p v-html="props.resultDetail"></p>
-      </ParagraphText>
-      <ParagraphText v-show="props.credit" class="kjfhsdkfhksjdfhkjshwuef">
-        <br />
-        *เนื้อหาดังกล่าวเขียนขึ้นจากการค้นคว้าเอกสารและเรื่องเล่าเท่านั้น<br />
-        ที่มาของภาพ: <a :href="props.creditLink" target="_blank" rel="noopener noreferrer" class="underline">{{ props.credit }}</a>
-      </ParagraphText>
+      <transition name="name-fade" appear>
+        <h1 class="font-Prachachon whitespace-pre text-7xl leading-13 sm:(text-5rem leading-15) lg:(text-7xl) xl:(text-8xl) 2xl:(text-9xl)">
+          {{ props.resultFullName }}
+        </h1>
+      </transition>
+      <transition name="detail-fade" appear>
+        <ParagraphText class="kjfhsdkfhksjdfhkjshwuef">
+          <p v-html="props.resultDetail"></p>
+        </ParagraphText>
+      </transition>
+      <transition name="disclaimer-fade" appear>
+        <ParagraphText v-show="props.credit" class="kjfhsdkfhksjdfhkjshwuef">
+          <br />
+          *เนื้อหาดังกล่าวเขียนขึ้นจากการค้นคว้าเอกสารและเรื่องเล่าเท่านั้น<br />
+          ที่มาของภาพ: <a :href="props.creditLink" target="_blank" rel="noopener noreferrer" class="underline">{{ props.credit }}</a>
+        </ParagraphText>
+      </transition>
     </div>
   </div>
 </template>
@@ -28,5 +34,24 @@ const props = defineProps<{
 <style>
 .kjfhsdkfhksjdfhkjshwuef {
   @apply whitespace-pre-wrap mt-6 font-light text-lg sm:(text-xl leading-8) lg:(text-xl leading-8 mt-3) xl:() 2xl:(text-2xl leading-9);
+}
+
+.name-fade-enter-active,
+.detail-fade-enter-active,
+.disclaimer-fade-enter-active {
+  transition: all 0.7s ease-out;
+}
+.name-fade-enter-from,
+.detail-fade-enter-from,
+.disclaimer-fade-enter-from {
+  transform: translateY(20px);
+  opacity: 0;
+}
+
+.detail-fade-enter-active {
+  transition-delay: .5s;
+}
+.disclaimer-fade-enter-active {
+  transition-delay: 1.2s;
 }
 </style>
