d64a4b8cb9ceb31acc9bf33c4d8e42993b07e9da
Arm <69001162+arskiir@users.noreply.github.com>
2021-10-12 21:44:02 +0700

    ✨ fullscreen on app mounting

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,6 @@
 <script setup lang="ts">
-// https://github.com/vueuse/head
-// you can use this to manipulate the document head in any components,
-// they will be rendered correctly in the html results with vite-ssg
-// useHead({
-//   title: 'Vitesse',
-//   meta: [
-//     { name: 'description', content: 'Opinionated Vite Starter Template' },
-//   ],
-// })
+const { enter, isSupported } = useFullscreen()
+onMounted(() => isSupported && enter())
 </script>
 
 <template>
